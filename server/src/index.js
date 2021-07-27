d50388a720183fcd3ac61632553d674a9f2e566d
tupe12334 <ofek.gabay.he@gmail.com>
2021-07-27 11:18:10 +0300

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,47 +1,20 @@
-const express = require("express");
+
 const path = require("path");
-const app = express(),
-  bodyParser = require("body-parser");
 const port = 8080;
 const relpath = "../../client/build/"
-// place holder for the data
-const users = [
-  {
-    firstName: "first1",
-    lastName: "last1",
-    email: "abc@gmail.com",
-  },
-  {
-    firstName: "first2",
-    lastName: "last2",
-    email: "abc@gmail.com",
-  },
-  {
-    firstName: "first3",
-    lastName: "last3",
-    email: "abc@gmail.com",
-  },
-];
 
-app.use(bodyParser.json());
-app.use(express.static(process.cwd() + relpath));
+const express = require('express');
+const app = express();
 
-app.get("/api/users", (req, res) => {
-  console.log("api/users called!");
-  res.json(users);
+app.use(express.static(path.join(__dirname, relpath)));
+
+app.use((req, res) => {
+  res.status(200).send('Hello, world!');
 });
 
-app.post("/api/user", (req, res) => {
-  const user = req.body.user;
-  console.log("Adding user:::::", user);
-  users.push(user);
-  res.json("user addedd");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + relpath + "/index.html");
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on the port::${port}`);
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
